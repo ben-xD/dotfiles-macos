9504fedccbe36f2ef947099a162b54101a03aaf5
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-10-11 16:34:47 +0100

    add setup script

---
+++ .config/.dotfiles/setup.sh
@@ -0,0 +1,12 @@
+# First time machine setup script.
+
+# install oh-my-zsh
+
+
+# install oh-my-zsh plugins: git zsh-autosuggestions asdf z
+
+# install powerlevel10k
+
+# Set up symlinks configs
+## nvim
+
