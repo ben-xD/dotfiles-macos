da576e5b943d51600cfd1208759a3cd2354b55ad
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-10-13 00:32:15 +0100

    add macOS configuration from https://gist.github.com/bradp/bea76b16d3325f5c47d4

--- .config/dotfiles/setup.sh
+++ .config/dotfiles/setup.sh
@@ -4,6 +4,7 @@ if test -f "/$HOME/.ssh/github"; then
   echo "create: creating GitHub ssh key with no passphrase."
   ssh-keygen -f "$HOME/.ssh/github" -N ""
   echo "Add the key to GitHub: https://github.com/settings/keys"
+  read -p "Press [Enter] to continue..."
 else
   echo "skip: github ssh key already exists"
 fi
@@ -13,7 +14,8 @@ xcode-select --install
 
 if test ! $(which brew); then
 else
-  echo "skip: brew is already installed"
+  echo "skip: brew is already installed, updating instead."
+  brew update
 fi
 
 echo "install: oh-my-zsh, as per https://ohmyz.sh/#install"
@@ -23,6 +25,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "install: zsh plugins"
 # as per https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# TODO Consider zoxide?: https://github.com/ajeetdsouza/zoxide
 git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
@@ -37,32 +40,24 @@ ln -s "$HOME/.config/dotfiles/nvim-custom/" "$HOME/.config/nvim/lua/custom"
 
 ## pyenv
 brew install pyenv
+pyenv install 3.11
+pyenv global 3.11
 
 ## nvm
 brew install nvm
 
-echo "Install apps via brew. Find more casks on https://formulae.brew.sh/cask/"
-brew install --cask rectangle
-brew install --cask itsycal
-brew install --cask alfred
-brew install --cask visual-studio-code
-brew install --cask cleanshot
-brew install --cask firefox
-brew install --cask docker
-brew install --cask figma
-brew install --cask obsidian
-brew install --cask fork
-brew install --cask google-chrome
-brew install --cask qlmarkdown
-brew install --cask yubico-yubikey-manager
-brew install --cask iterm2
-brew install --cask logi-options-plus
-brew install --cask typora
-brew install --cask trailer
-brew install --cask slack
-# Need a custom cask? see https://github.com/Homebrew/homebrew-cask/blob/c1bc489c27f061871660c902c89a250a621fb7aa/Casks/e/eagle.rb
+## More dev tools
+brew install htop
+brew install legit
+# brew install git-flow
+brew install git-extras
+brew install tree
+brew install wget
+brew install trash
+brew install mackup
 
-echo "Manually install apps: Eagle.cool, "
+## TODO Consider more tools?
+# brew install coreutils curl git openssl readline sqlite3 xz zlib tcl-tk # needed for asdf and asdf-python
 
 # GPG set up
 # prompt for smartcard to be added
@@ -70,8 +65,236 @@ GPG_PUBLIC_KEY_ID="0x5FC80BAF2B00A4F9 2023-10-06"
 brew install gnupg pinentry-mac
 # Create subkey from GPG key on yubikey/smartcard
 echo "manual(GitHub): Delete existing GPG public key and add new public key, containing subkey used by this machine."
+read -p "Press [Enter] to continue..."
 
 echo "Add licenses to apps: alfred, cleanshot, fork"
-echo "Copy configuration of Alfred"
+read -p "Press [Enter] to continue..."
 
-echo "Finally, set up .secrets.env containing machine or organisation specific secrets, e.g. Artifactory credentials, Cloudflare credentials or device identifiers."
+echo "Copy configuration of Alfred"
+read -p "Press [Enter] to continue..."
+
+echo "Login to browsers and browser extensions"
+read -p "Press [Enter] to continue..."
+
+echo "Manually install apps: Eagle.cool"
+read -p "Press [Enter] to continue..."
+
+echo "Set up .secrets.env containing machine or organisation specific secrets, e.g. Artifactory credentials, Cloudflare credentials or device identifiers."
+touch "$HOME/.secrets.env"
+
+echo "install: apps via brew. Find more casks on https://formulae.brew.sh/cask/"
+# Need a custom cask? see https://github.com/Homebrew/homebrew-cask/blob/c1bc489c27f061871660c902c89a250a621fb7aa/Casks/e/eagle.rb
+apps=(
+  rectangle
+  itsycal
+  alfred
+  visual-studio-code
+  cleanshot
+  firefox
+  docker
+  figma
+  obsidian
+  fork
+  google-chrome
+  qlmarkdown
+  qlstephen
+  yubico-yubikey-manager
+  iterm2
+  logi-options-plus
+  typora
+  trailer
+  slack
+  bartender
+  vlc
+)
+# Install apps to /Applications
+# Default is: /Users/$user/Applications
+brew install --cask --appdir="/Applications" ${apps[@]}
+
+# More apps to try:
+  # bettertouchtool
+  # cleanmymac
+  # cloud
+  # colloquy
+  # cornerstone
+  # diffmerge
+  # harvest
+  # hipchat
+  # licecap
+  # mou
+  # razer-synapse
+  # sublime-text2
+  # textexpander
+  # transmission
+  # sequel-pro
+  # chromecast
+  # suspicious-package
+
+brew cask alfred link
+
+brew cask cleanup
+brew cleanup
+
+echo "[DISABLED] Restoring setup from Mackup..."
+#mackup restore @TODO uncomment
+
+echo "Setting some macOS settings..."
+read -p "Press [Enter] to continue..."
+
+#"Disabling system-wide resume"
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+
+#"Disabling automatic termination of inactive apps"
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+
+#"Allow text selection in Quick Look"
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE
+
+#"Disabling OS X Gate Keeper"
+#"(You'll be able to install any app you want from here on, not just Mac App Store apps)"
+sudo spctl --master-disable
+sudo defaults write /var/db/SystemPolicy-prefs.plist enabled -string no
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+#"Expanding the save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+#"Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+#"Saving to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+#"Check for software updates daily, not just once per week"
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+#"Disable smart quotes and smart dashes as they are annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+
+#"Enabling full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+#"Disabling press-and-hold for keys in favor of a key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+#"Setting trackpad & mouse speed to a reasonable number"
+defaults write -g com.apple.trackpad.scaling 2
+defaults write -g com.apple.mouse.scaling 2.5
+
+#"Enabling subpixel font rendering on non-Apple LCDs"
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
+
+#"Showing icons for hard drives, servers, and removable media on the desktop"
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+
+#"Showing all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+#"Disabling the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+#"Use column view in all Finder windows by default"
+defaults write com.apple.finder FXPreferredViewStyle Clmv
+
+#"Avoiding the creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+#"Enabling snap-to-grid for icons on the desktop and in other icon views"
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+#"Setting the icon size of Dock items to 36 pixels for optimal size/screen-realestate"
+defaults write com.apple.dock tilesize -int 36
+
+#"Speeding up Mission Control animations and grouping windows by application"
+defaults write com.apple.dock expose-animation-duration -float 0.1
+defaults write com.apple.dock "expose-group-by-app" -bool true
+
+#"Setting Dock to auto-hide and removing the auto-hiding delay"
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+#"Setting email addresses to copy as 'foo@example.com' instead of 'Foo Bar <foo@example.com>' in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
+#"Enabling UTF-8 ONLY in Terminal.app and setting the Pro theme by default"
+defaults write com.apple.terminal StringEncodings -array 4
+defaults write com.apple.Terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.Terminal "Startup Window Settings" -string "Pro"
+
+#"Preventing Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+#"Disable the sudden motion sensor as its not useful for SSDs"
+sudo pmset -a sms 0
+
+#"Speeding up wake from sleep to 24 hours from an hour"
+# http://www.cultofmac.com/221392/quick-hack-speeds-up-retina-macbooks-wake-from-sleep-os-x-tips/
+sudo pmset -a standbydelay 86400
+
+#"Disable annoying backswipe in Chrome"
+defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
+
+#"Setting screenshots location to ~/Desktop"
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+
+#"Setting screenshot format to PNG"
+defaults write com.apple.screencapture type -string "png"
+
+#"Hiding Safari's bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+#"Hiding Safari's sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+#"Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+#"Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+#"Making Safari's search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+#"Removing useless icons from Safari's bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+#"Allow hitting the Backspace key to go to the previous page in history"
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+#"Enabling the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+
+#"Adding a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+#"Use `~/Downloads/Incomplete` to store incomplete downloads"
+defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
+
+#"Don't prompt for confirmation before downloading"
+defaults write org.m0k.transmission DownloadAsk -bool false
+
+#"Trash original torrent files"
+defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+#"Hide the donate message"
+defaults write org.m0k.transmission WarningDonate -bool false
+
+#"Hide the legal disclaimer"
+defaults write org.m0k.transmission WarningLegal -bool false
+
+#"Disable 'natural' (Lion-style) scrolling"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+killall Finder
