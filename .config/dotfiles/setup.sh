fe7243e8a722b26412cff8bba641cffc0cd882b7
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-10-13 15:00:15 +0100

    fix reload shell for brew

--- .config/dotfiles/setup.sh
+++ .config/dotfiles/setup.sh
@@ -9,7 +9,7 @@ xcode-select --install || true
 echo "install: brew. Why? It helps us install more apps and developer tools without manually downloading files."
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  exec $SHELL # so we can use brew
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "skip: brew is already installed, updating instead."
   brew update
